0bee5df70021b4b91308c114ee938b7f31059125
Miguel-Tangarife <angel.tangarife@udea.edu.co>
2023-07-26 23:48:41 -0500

    Code added

---
+++ code/road_distance_euclidean.R
@@ -0,0 +1,13 @@
+#Raster Euclidean Distance Calculatio#
+
+install.packages("raster")
+install.packages("geosphere")
+install.packages("rgdal")
+library(raster)
+library(geosphere)
+library(rgdal)
+
+
+#Definir la resolución de pixeles (1km)
+extent <- read()
+resolucion <- 0.01
